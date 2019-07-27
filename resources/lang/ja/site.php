e837f3570f8e9ef1c6bf6210a55f130cf7835eb5
YoheiZuho <yoheizuho@github.com>
2019-07-28 01:16:23 +0900

    update japanese translation

--- resources/lang/ja/site.php
+++ resources/lang/ja/site.php
@@ -6,7 +6,7 @@ return [
 	'fediverse'     => '分散',
 	'opensource'	=> 'オープンソース',
 	'terms'         => '利用規約',
-	'privacy'       => 'プライバシー',
+	'privacy'       => 'プライバシーポリシー',
 	'l10nWip'       => '私達はローカライズサポートに取り組んでいます',
 	'currentLocale' => '現在のロケール',
 	'selectLocale'  => 'リストの中からロケール（言語）を選択してください',
